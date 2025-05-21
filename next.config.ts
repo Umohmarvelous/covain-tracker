cbcebb6fd48d1659714c46479f9673a2ce5ac078
Umohmarvelous <umohm450@gmail.com>
2025-05-21 11:12:18 +0100

    Fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  base: process.env.NEXT_BASE_PATH || '/react-expense-tracker'
 };
 
 export default nextConfig;
