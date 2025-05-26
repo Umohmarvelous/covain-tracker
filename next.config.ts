f0ba2898d46883d16caada7e5a47a93c6eaa139b
Umohmarvelous <umohm450@gmail.com>
2025-05-26 10:20:16 +0100

    Fix

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // base: process.env.NEXT_BASE_PATH || '/react-expense-tracker',
-  // eslint: {
-  //   ignoreDuringBuilds: true,
-  // },
-  // typescript: {
-  //   ignoreBuildErrors: true,
-  // },
+  base: process.env.NEXT_BASE_PATH || '/react-expense-tracker',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       {
